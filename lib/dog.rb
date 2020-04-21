f388152c7fc1e19a766ff98b08afa2419853ad07
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-21 13:48:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,7 +82,7 @@ class Dog
     sql = <<-SQL
     SELECT * FROM dogs WHERE id = ?
     SQL
-
+S
     row = DB[:conn].execute(sql, id).collect{ |row|
       self.new_from_db(row)
     }.first
@@ -94,5 +94,6 @@ class Dog
     binding.pry
     if
     end
-  end
+  endS
+  
 end
