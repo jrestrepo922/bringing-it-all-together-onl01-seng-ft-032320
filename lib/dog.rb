c608f30809db575dcfb35963a4eadad12d98509e
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-21 12:19:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,11 +23,11 @@ class Dog
 
   def self.drop_table
     sql = <<-SQL
-    DROP TABLE IF EXISTS dogs; 
+    DROP TABLE IF EXISTS dogs;
     SQL
 
     DB[:conn].execute(sql)
-  end 
+  end
 
-  
+
 end
