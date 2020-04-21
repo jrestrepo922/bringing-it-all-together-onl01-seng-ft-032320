42814b19bd37741eabbc944d25a6224fbebd2a76
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-21 13:40:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,10 +6,8 @@ class Dog
 
 
   def initialize(attributes)
-    attributes.each {|key, value|
-      self.send(("#{key}="), value)
-    }
-    self.id || self.id = nil
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
   end
 
   def self.create_table
