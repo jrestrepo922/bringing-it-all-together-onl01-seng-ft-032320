b415fb99072708e707d8ee18982d9c4cd4b055b3
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-21 13:52:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -89,7 +89,7 @@ class Dog
 
   end
 
-  def self.find_or_create_by(name, breed)
+  def self.find_or_create_by(hash)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?;", name, breed)
     binding.pry
   end
