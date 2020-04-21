18b9b96d6caaa5e015ba377dd5a34bfdd92b9ded
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-21 13:49:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -90,10 +90,8 @@ class Dog
   end
 
   def self.find_or_create_by(name, breed)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?", name, breed)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?;", name, breed)
     binding.pry
-    if
-    end
   end
 
 end
